e65d9044263f84bd06b98977a6661147cd6fe562
Jan-Derek Vos <j.vos@bosqom.nl>
2014-06-10 04:48:06 -0400

    - property_vge_type_id filter

--- CRM/Mbreports/Form/Report/WerkoverzichtDossier.php
+++ CRM/Mbreports/Form/Report/WerkoverzichtDossier.php
@@ -668,7 +668,7 @@ class CRM_Mbreports_Form_Report_WerkoverzichtDossier extends CRM_Report_Form {
       $where = " WHERE ";
       foreach($this->formFilter as $field => $filter){
         
-        if('case_type_id' == $field or 'property_vge_type' == $field){
+        if('case_type_id' == $field or 'property_vge_type_id' == $field){
           $where .= " ( " . $field . " LIKE CONCAT ('%" . CRM_Core_DAO::VALUE_SEPARATOR . "'," . $filter['value'] . ",'" . CRM_Core_DAO::VALUE_SEPARATOR . "%') ) AND ";
           
         }else if (CRM_Report_Form::OP_DATE == $filter['operatorType']) {
@@ -824,7 +824,9 @@ class CRM_Mbreports_Form_Report_WerkoverzichtDossier extends CRM_Report_Form {
         $dao_labels = CRM_Core_DAO::executeQuery($sql);
         $dao_labels->fetch();
         
-        $labels[] = $dao_labels->label;
+        if(!empty($dao_labels->label)){
+          $labels[] = $dao_labels->label;
+        }
       }
       
       $sql = "UPDATE werkoverzicht_dossier SET typeringen = '" . addslashes(implode(',', $labels)) . "' WHERE case_id = '" . $dao->entity_id . "'";
